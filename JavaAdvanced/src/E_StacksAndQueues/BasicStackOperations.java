6bf5e6db6e0b153723616d9699f05b401b3db1cd
Radost Koleva <radostkoleva0142@gmail.com>
2023-08-25 15:40:56 +0300

    exercises part 1

---
+++ JavaAdvanced/src/E_StacksAndQueues/BasicStackOperations.java
@@ -0,0 +1,4 @@
+package E_StacksAndQueues;
+
+public class BasicStackOperations {
+}
--- JavaAdvanced/src/E_StacksAndQueues/ReverseNumbers.java
+++ JavaAdvanced/src/E_StacksAndQueues/ReverseNumbers.java
@@ -1,4 +1,22 @@
 package E_StacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class ReverseNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] inputArr = scan.nextLine().split(" ");
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (String el : inputArr) {
+            int elAsNum = Integer.parseInt(el);
+            stack.push(elAsNum);
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.printf("%d ", stack.pop());
+        }
+    }
 }
