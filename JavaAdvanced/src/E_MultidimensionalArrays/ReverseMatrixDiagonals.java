f0107aad63f64cdf37b96a33d9b4aff3406b4c84
Radost Koleva <radostkoleva0142@gmail.com>
2023-09-06 23:10:20 +0300

    Sets and maps and multidimensional arrays exercises

---
+++ JavaAdvanced/src/E_MultidimensionalArrays/ReverseMatrixDiagonals.java
@@ -0,0 +1,41 @@
+package E_MultidimensionalArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseMatrixDiagonals {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] size = Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int rows = size[0];
+        int cols = size[1];
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            int[] curRow = Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            matrix[i] = curRow;
+        }
+
+        int row = rows - 1;
+        int col = cols - 1;
+
+        while (row != -1){
+            int r = row;
+            int c = col;
+
+            while (c < cols && r >= 0){
+                //first is printing and then values are decreased.
+                System.out.print(matrix[r--][c++] + " ");
+            }
+
+            System.out.println();
+            col--;
+
+            if(col == -1){
+                col = 0;
+                row--;
+            }
+        }
+    }
+}
---
+++ JavaAdvanced/src/E_SetsAndMapsAdvanced/CountSymbols.java
@@ -0,0 +1,27 @@
+package E_SetsAndMapsAdvanced;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class CountSymbols {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        TreeMap<Character, Integer> map = new TreeMap<>();
+        String input = scan.nextLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            char curSymbol = input.charAt(i);
+
+            if (!map.containsKey(curSymbol)) {
+                map.put(curSymbol, 1);
+            } else {
+                int oldCount = map.get(curSymbol);
+                map.put(curSymbol, oldCount + 1);
+            }
+        }
+
+        map.forEach((key, value) -> {
+            System.out.printf("%c: %d time/s\n", key, value);
+        });
+    }
+}
