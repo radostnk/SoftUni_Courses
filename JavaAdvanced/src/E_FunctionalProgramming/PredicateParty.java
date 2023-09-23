be4d43eb4abf06e8bfa330c43475cee33b03ddee
Radost Koleva <radostkoleva0142@gmail.com>
2023-09-23 22:07:05 +0300

    Functional programming more exercises

---
+++ JavaAdvanced/src/E_FunctionalProgramming/PredicateParty.java
@@ -0,0 +1,10 @@
+package E_FunctionalProgramming;
+
+import java.util.Scanner;
+
+public class PredicateParty {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
