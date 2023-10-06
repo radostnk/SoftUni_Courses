a64ebe5a894da02a7d43e0ca51be842adcca5fa2
Radost Koleva <radostkoleva0142@gmail.com>
2023-10-06 15:35:33 +0300

    Iterators and comparators exercises

---
+++ JavaAdvanced/src/E_IteratorsAndComparators/MovieStore/Shelf.java
@@ -0,0 +1,45 @@
+package E_IteratorsAndComparators.MovieStore;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Shelf implements Iterable<String>{
+    private List<String> movieTitles;
+
+    public Shelf(String... movieTitles) {
+        this.movieTitles = Arrays.asList(movieTitles);
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return new ShelfIterator();
+
+//        return new Iterator<String>() {
+//            int index = 0;
+//
+//            @Override
+//            public boolean hasNext() {
+//                return index < movieTitles.size();
+//            }
+//
+//            @Override
+//            public String next() {
+//                return movieTitles.get(index++);
+//            }
+//        };
+    }
+
+    public class ShelfIterator implements Iterator<String>{
+        int index = 0;
+        @Override
+        public boolean hasNext() {
+            return index < movieTitles.size();
+        }
+
+        @Override
+        public String next() {
+            return movieTitles.get(index++);
+        }
+    }
+}
---
+++ JavaAdvanced/src/E_IteratorsAndComparators/StackIterator/Stack.java
@@ -0,0 +1,48 @@
+package E_IteratorsAndComparators.StackIterator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class Stack implements Iterable<Integer> {
+    private List<Integer> myStack;
+
+    public Stack() {
+        this.myStack = new ArrayList<>();
+    }
+
+    public void push(Integer item) {
+        this.myStack.add(item);
+    }
+
+    public Integer pop() {
+        int itemPopped = myStack.get(myStack.size() - 1);
+        myStack.remove(myStack.size() - 1);
+        return itemPopped;
+    }
+
+    public boolean isNotEmpty() {
+        return myStack.size() > 0;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            int index = myStack.size() - 1;
+
+            @Override
+            public boolean hasNext() {
+                return index >= 0;
+            }
+
+            @Override
+            public Integer next() {
+                int next = myStack.get(index);
+                index--;
+                return next;
+            }
+        };
+    }
+}
